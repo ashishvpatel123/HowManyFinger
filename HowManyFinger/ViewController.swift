19afa4a7a9cb99acdd39f3a3c74b88510d572ea1
IMCS2 <IMCS2@imcs1s-macbook-air-4.local>
2019-02-07 23:06:32 -0600

    created simple how many finger game

--- HowManyFinger/ViewController.swift
+++ HowManyFinger/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOfFingers: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var ansLable: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func submitclicked(_ sender: Any) {
+        
+        let randomNumber = Int.random(in: 1...10)
+        let _userSelectedNumber =  Int(numberOfFingers.text!)
+        if _userSelectedNumber == randomNumber{
+            imageView.image = UIImage(named: "winer")
+        }else{
+            imageView.image = UIImage(named: "looser")
+        }
+        
+        ansLable.text = "Phone Guessed \(randomNumber)"
+    
+        print("submit Buton clicked")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        submitButton.layer.cornerRadius = 10
+        submitButton.clipsToBounds = true
+        
+        
+        
     }
 
 
